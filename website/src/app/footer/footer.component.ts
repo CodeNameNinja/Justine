bae0b89c18f415a77e2913899e430b5624705d42
Mitchell Yuen <35192626+CodeNameNinja@users.noreply.github.com>
2020-05-11 12:14:36 +0200

    modified home screen look

--- website/src/app/footer/footer.component.ts
+++ website/src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { ShopService } from '../services/shop.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    private shopService: ShopService
+  ) { }
 
   ngOnInit() {
+  }
+
+  filter(category:string){
+
+    this.shopService.filterCategories.next(category);
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 
 }
